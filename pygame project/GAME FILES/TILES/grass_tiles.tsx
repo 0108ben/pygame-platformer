fec9ec6c0758bc87ae7aa63b3d3c6d3a53cc6052
0108ben <121033753+0108ben@users.noreply.github.com>
2023-01-23 09:29:20 +0000

    Delete pygame project directory
    Found a way to convert it into an exe file

--- pygame project/GAME FILES/TILES/grass_tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.6" name="grass_tiles" tilewidth="64" tileheight="64" tilecount="5" columns="5">
- <image source="../../../../../OneDrive/Documents/pygame graphic stuff/graphics/decoration/grass/grass.png" width="320" height="64"/>
-</tileset>
